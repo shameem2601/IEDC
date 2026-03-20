0f9ea3c675601439a3ca08660398fa33e05573ae
Shameem <shameemkingyt@gmail.com>
2026-03-20 20:49:09 +0530

    Integrate Firebase Analytics and initialization

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB10kzJWld9P9f378suJChtyYMDeHNAAL0",
+  authDomain: "iedc-website-2.firebaseapp.com",
+  projectId: "iedc-website-2",
+  storageBucket: "iedc-website-2.firebasestorage.app",
+  messagingSenderId: "889411924349",
+  appId: "1:889411924349:web:39d7ee7728564d394d5c14",
+  measurementId: "G-TVHVYDE55B"
+};
+
+const app = initializeApp(firebaseConfig);
+export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
